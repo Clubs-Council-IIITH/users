2c8492d9007a1fff5eb7b19dd9f356c49c4113bd
v15hv4 <vishva2912@gmail.com>
2023-07-10 01:12:21 +0530

    Add mutation to update profile image

--- mutations.py
+++ mutations.py
@@ -5,7 +5,7 @@ from fastapi.encoders import jsonable_encoder
 from db import db
 
 # import all models and types
-from otypes import Info, RoleInput
+from otypes import ImageInput, Info, RoleInput
 from models import User
 
 
@@ -28,13 +28,30 @@ def updateRole(roleInput: RoleInput, info: Info) -> bool:
     if not db_user:
         new_user = User(uid=roleInput["uid"])
         db.users.insert_one(jsonable_encoder(new_user))
-    
+
     # update role in database
     db.users.update_one(
         {"uid": roleInput["uid"]},
         {"$set": {"role": roleInput["role"]}},
     )
-        
+
+    return True
+
+
+@strawberry.mutation
+def updateImage(imgInput: ImageInput, info: Info) -> bool:
+    user = info.context.user
+    if not user:
+        raise Exception("Not logged in!")
+
+    imgInput = jsonable_encoder(imgInput)
+
+    db_user = db.users.find_one({"uid": imgInput["uid"]})
+
+    db.users.update_one(
+        {"uid": imgInput["uid"]},
+        {"$set": {"img": imgInput["img"]}},
+    )
 
     return True
 
@@ -42,4 +59,5 @@ def updateRole(roleInput: RoleInput, info: Info) -> bool:
 # register all mutations
 mutations = [
     updateRole,
+    updateImage,
 ]
