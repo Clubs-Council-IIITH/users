b0933bf29220464d6e7b0469c4fcefc767d8f8cb
Bhav Beri <bhavberi@gmail.com>
2026-03-04 12:35:54 +0530

    No need of graphql ide for individual subgraph (as we use gateway)

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ schema = strawberry.federation.Schema(
 DEBUG = getenv("GLOBAL_DEBUG", "False").lower() in ("true", "1", "t")
 
 # serve API with FastAPI router
-gql_app = GraphQLRouter(schema, graphql_ide="graphiql", context_getter=get_context)
+gql_app = GraphQLRouter(schema, context_getter=get_context)
 app = FastAPI(debug=DEBUG)
 app.include_router(gql_app, prefix="/graphql")
